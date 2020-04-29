fd4d75a6dd4ec15e94d63c6ad43b9e1998585fed
alexB18 <abrown15@uoregon.edu>
2020-04-28 22:44:49 -0700

    Added project 1 part1 main file skeleton.

---
+++ projects/project2/mcp1.c
@@ -0,0 +1,32 @@
+/*
+* Description:
+*
+* Author: Alex Brown
+*
+* Date: 4/28/2020
+*
+* Notes:
+*/
+
+/*-------------------------Preprocessor Directives---------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+/*---------------------------------------------------------------------------*/
+
+int main(int argc, char** argv){
+
+    /* 1. Read the program workload from the specified input file. */
+
+    /* 2. For each program, MCP mush launch program to run as separate process
+    using some variant of fork(2) and one of the exec() system calls. */
+
+    /* 3. Once all of the programs are running, MCP must wait for each program to
+    terminate using the system call wait(). */
+
+    /* 4. After all programs have terminated, MCP must exit using the exit() 
+    system call. */
+
+}
