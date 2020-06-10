330648573dbd966049b19b8ca7871cbeb0199358
alexB18 <abrown15@uoregon.edu>
2020-06-09 23:12:48 -0700

    Adding merge so I can undo a commit??

--- projects/project3/topics.c
+++ projects/project3/topics.c
@@ -4,93 +4,54 @@
 #include <stdlib.h>
 #include "topics.h"
 
-/* --------------- TopicEntry Setters --------------- */
-void setEntryTimeStamp(TopicEntry* Entry, struct timeval* newTimeStamp){
+/* --------------- topicEntry Setters --------------- */
+void setEntryTimeStamp(topicEntry** Entry, struct timeval* newTimeStamp){
 
-    memcpy(&(Entry)->timeStamp, newTimeStamp, sizeof(*newTimeStamp));
+    memcpy(&(*Entry)->timeStamp, newTimeStamp, sizeof(*newTimeStamp));
 }
 
-void setEntryNum(TopicEntry* Entry, int newEntryNum){
-    Entry->entryNum = newEntryNum;
+void setEntryNum(topicEntry** Entry, int newEntryNum){
+    (*Entry)->entryNum = newEntryNum;
 }
 
-void setEntryPubID(TopicEntry* Entry, int newPubID){
-    Entry->pubID = newPubID;
+void setEntryPubID(topicEntry** Entry, int newPubID){
+    (*Entry)->pubID = newPubID;
 }
 
-void setEntryPhotoURL(TopicEntry* Entry, char** newPhotoURL){
-
-    // First free old photoURL
-    free(Entry->photoURL);
-
-    // Then reallocated and assign
-    Entry->photoURL = (char*) malloc(URLSIZE * sizeof(char));
-    strcpy((Entry->photoURL), *newPhotoURL);
+void setEntryPhotoURL(topicEntry** Entry, char** newPhotoURL){
+    strcpy((*Entry)->photoURL, *newPhotoURL);
 }
 
-void setEntryPhotoCaption(TopicEntry* Entry, char** newPhotoCaption){
-    strcpy(Entry->photoCaption, *newPhotoCaption);
-}
-
-void initializeEntry(TopicEntry** Entry, int newEntryNum, int newPubID){
-    
-    // Start by allocating memory for the new Entry...
-    *Entry = (TopicEntry*) malloc(sizeof(TopicEntry));
-
-    // Next, allocate memory for both the photoURL and the photoCaption
-    (*Entry)->photoURL = (char*) malloc(URLSIZE * sizeof(char));
-    (*Entry)->photoCaption = (char*) malloc(CAPSIZE * sizeof(char));
-
-    // Retrieve current time
-    struct timeval entryTimeStamp;
-    gettimeofday(&entryTimeStamp, NULL);
-    setEntryTimeStamp(*Entry, &entryTimeStamp);
-
-    // Set new entry num
-    setEntryNum(*Entry, newEntryNum);
-
-    // Set new pub id
-    setEntryPubID(*Entry, newPubID);
-
-}
-
-void freeEntry(TopicEntry* Entry){
-
-    // Start by freeing memory from both the photoURL and the photoCaption
-    free(Entry->photoURL);
-    free(Entry->photoCaption);
-
-    // Finally, free self
-    free(Entry);
-
+void setEntryPhotoCaption(topicEntry** Entry, char** newPhotoCaption){
+    strcpy((*Entry)->photoCaption, *newPhotoCaption);
 }
 /* -------------------------------------------------- */
 
 
-/* --------------- TopicEntry Getters --------------- */
-struct timeval getEntryTimeStamp(TopicEntry* Entry){
-    return Entry->timeStamp;
+/* --------------- topicEntry Getters --------------- */
+struct timeval getEntryTimeStamp(topicEntry** Entry){
+    return (*Entry)->timeStamp;
 }
 
-int getEntryNum(TopicEntry* Entry){
-    return Entry->entryNum;
+int getEntryNum(topicEntry** Entry){
+    return (*Entry)->entryNum;
 }
 
-int getEntryPubID(TopicEntry* Entry){
-    return Entry->pubID;
+int getEntryPubID(topicEntry** Entry){
+    return (*Entry)->pubID;
 }
 
-char* getEntryPhotoURL(TopicEntry* Entry){
-    return Entry->photoURL;
+char* getEntryPhotoURL(topicEntry** Entry){
+    return (*Entry)->photoURL;
 }
 
-char* getEntryPhotoCaption(TopicEntry* Entry){
-    return Entry->photoCaption;
+char* getEntryPhotoCaption(topicEntry** Entry){
+    return (*Entry)->photoCaption;
 }
 /* -------------------------------------------------- */
 
-/* --------------- TopicEntry Helpers --------------- */
-void printEntryStatus(TopicEntry* Entry){
+/* --------------- topicEntry Helpers --------------- */
+void printEntryStatus(topicEntry** Entry){
 
     // Print Entry time stamp in seconds.milliseconds
     fprintf(stdout, "Time: %li.%li s\n", getEntryTimeStamp(Entry).tv_sec, getEntryTimeStamp(Entry).tv_usec);
@@ -99,66 +60,5 @@ void printEntryStatus(TopicEntry* Entry){
     fprintf(stdout, "Entry Photo URL: %s\n", getEntryPhotoURL(Entry));
     fprintf(stdout, "Entry Caption: %s\n", getEntryPhotoCaption(Entry));
     fprintf(stdout, "\n");
-}
-/* -------------------------------------------------- */
-
-/* --------------- TopicQueue Setters --------------- */
-void setTopicQueueHead(TopicQueue* Queue, TopicEntry** NewHead){
-    Queue->head = (TopicEntry*) *NewHead;
-}
-
-void setTopicQueueTail(TopicQueue* Queue, TopicEntry** NewTail){
-    Queue->tail = (TopicEntry*) *NewTail;
-}
-
-void incrementNumberOfTopics(TopicQueue* Queue){
-    Queue->numberOfTopics ++;
-}
-/* -------------------------------------------------- */
-
-/* --------------- TopicQueue Getters --------------- */
-TopicEntry* getTopicQueueHead(TopicQueue* Queue){
-    return Queue->head;
-}
-
-TopicEntry* getTopicQueueTail(TopicQueue* Queue){
-    return Queue->tail;
-}
-
-
-int getNumberOfTopics(TopicQueue* Queue){
-    return Queue->numberOfTopics;
-}
-
-/* -------------------------------------------------- */
-
-/* --------------- TopicQueue Helpers --------------- */
-void initializeTopicQueue(TopicQueue** Queue){
-
-    // Allocate memory for topic Address Array
-    (*Queue)->topicAddressArray = (TopicEntry**) malloc(MAXENTRIES * sizeof(TopicEntry*));
-
-    // Allocate memory for the head and tail pointers
-    (*Queue)->head = (TopicEntry*) malloc(sizeof(TopicEntry));
-    (*Queue)->tail = (TopicEntry*) malloc(sizeof(TopicEntry));
-
-    // Set head and tail to null by default
-    (*Queue)->head = NULL;
-    (*Queue)->tail = NULL;
-
-}
-
-void freeTopicQueue(TopicQueue* Queue){
-
-    // Free head and tail
-    freeEntry(Queue->head);
-    freeEntry(Queue->tail);
-
-    // Free topic address array
-    for(int i = 0; i < MAXENTRIES; i++){
-        freeEntry(Queue->topicAddressArray[i]);
-    }
-    free(Queue->topicAddressArray);
-
 }
 /* -------------------------------------------------- */
--- projects/project3/topics.h
+++ projects/project3/topics.h
@@ -6,76 +6,30 @@
 #define CAPSIZE 512
 #endif
 
-#ifndef MAXENTRIES
-#define MAXENTRIES 10
-#endif
-
-#ifndef MAXTOPICS
-#define MAXTOPICS 5
-#endif
-
-typedef struct TopicEntry{
+typedef struct topicEntry{
     int entryNum;
     struct timeval timeStamp;
     int pubID;
-    char* photoURL; // URL to photo
-    char* photoCaption; // photo caption
+    char photoURL[URLSIZE]; // URL to photo
+    char photoCaption[CAPSIZE]; // photo caption
+} topicEntry;
 
-    // pointer to next TopicEntry in queue
-    struct TopicEntry* NextEntry;
-} TopicEntry;
-
-/* --------------- TopicEntry Setters --------------- */
-void setEntryTimeStamp(TopicEntry* Entry, struct timeval* newTimeStamp);
-void setEntryNum(TopicEntry* Entry, int newEntryNum);
-void setEntryPubID(TopicEntry* Entry, int newPubID);
-void setEntryPhotoURL(TopicEntry* Entry, char** newPhotoURL);
-void setEntryPhotoCaption(TopicEntry* Entry, char** newPhotoCaption);
-
-void initializeEntry(TopicEntry** Entry, int newEntryNum, int newPubID);
-void freeEntry(TopicEntry* Entry);
+/* --------------- topicEntry Setters --------------- */
+void setEntryTimeStamp(topicEntry** Entry, struct timeval* newTimeStamp);
+void setEntryNum(topicEntry** Entry, int newEntryNum);
+void setEntryPubID(topicEntry** Entry, int newPubID);
+void setEntryPhotoURL(topicEntry** Entry, char** newPhotoURL);
+void setEntryPhotoCaption(topicEntry** Entry, char** newPhotoCaption);
 /* -------------------------------------------------- */
 
-/* --------------- TopicEntry Getters --------------- */
-struct timeval getEntryTimeStamp(TopicEntry* Entry);
-int getEntryNum(TopicEntry* Entry);
-int getEntryPubID(TopicEntry* Entry);
-char* getEntryPhotoURL(TopicEntry* Entry);
-char* getEntryPhotoCaption(TopicEntry* Entry);
+/* --------------- topicEntry Getters --------------- */
+struct timeval getEntryTimeStamp(topicEntry** Entry);
+int getEntryNum(topicEntry** Entry);
+int getEntryPubID(topicEntry** Entry);
+char* getEntryPhotoURL(topicEntry** Entry);
+char* getEntryPhotoCaption(topicEntry** Entry);
 /* -------------------------------------------------- */
 
-/* --------------- TopicEntry Helpers --------------- */
-void printEntryStatus(TopicEntry* Entry);
-/* -------------------------------------------------- */
-
-typedef struct TopicQueue{
-
-    // Array containing all address topics
-    // Will hold MAXENTRIES and will be used
-    // to implement queue
-    struct TopicEntry** topicAddressArray;
-
-    struct TopicEntry* head;
-    struct TopicEntry* tail;
-
-    int numberOfTopics;
-
-
-} TopicQueue;
-
-/* --------------- TopicQueue Setters --------------- */
-void setTopicQueueHead(TopicQueue* Queue, TopicEntry** NewHead);
-void setTopicQueueTail(TopicQueue* Queue, TopicEntry** NewTail);
-void incrementNumberOfTopics(TopicQueue* Queue);
-/* -------------------------------------------------- */
-
-/* --------------- TopicQueue Getters --------------- */
-TopicEntry* getTopicQueueHead(TopicQueue* Queue);
-TopicEntry* getTopicQueueTail(TopicQueue* Queue);
-int getNumberOfTopics(TopicQueue* Queue);
-/* -------------------------------------------------- */
-
-/* --------------- TopicQueue Helpers --------------- */
-void initializeTopicQueue(TopicQueue** Queue);
-void freeTopicQueue(TopicQueue* Queue);
+/* --------------- topicEntry Helpers --------------- */
+void printEntryStatus(topicEntry** Entry);
 /* -------------------------------------------------- */
